6d1729d8b7912a3c2dfd5cc6bb2afadb46e25337
ediskrad <ediskrad@lrrr.(none)>
2014-11-22 01:02:15 +0100

    Third commit

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 # Reading RDS object into dataframe
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-
+library(ggplot2)
 
 NEI <- transform(NEI, year = factor(year))
 #boxplot(Emissions ~ year, NEI, xlab = "Year", ylab = "Emissions")
@@ -37,8 +37,20 @@ totals_comb = aggregate(Emissions ~ year, NEI_comb, sum)
 png("plot4.png", width=480, height=480)
 qplot(year, Emissions, data=totals_comb) +
   labs(title="PM2.5 Emission from coal combustion-related sources ",
-       y="Total PM2.5 emission each year", x="Year")
+       y="Total PM2.5 emission each year", x="Year") + 
+  geom_point(size=4)
 
 dev.off()
 
+# Plot 5
+vehicles = as.vector(SCC[grep("vehicle",SCC$SCC.Level.Two,ignore.case=T),1])
+NEI_vehi = NEI[NEI$SCC %in% vehicles,]
+totals_vehi = aggregate(Emissions ~ year, data = subset(NEI_vehi, fips == "24510"), sum)
+png("plot5.png", width=480, height=480)
+qplot(year, Emissions, data=totals_vehi) +
+  labs(title="PM2.5 Emission from vehicles in Baltimore ",
+       y="Total PM2.5 emission each year", x="Year") +
+  geom_point(size=4)
+
+dev.off()
 
